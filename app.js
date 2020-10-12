2ab1c6b00f5b8b1b78be8641e56caa9a7701a735
blangwell <b.langwell@outlook.com>
2020-10-12 15:23:01 -0700

    [portfolio] Update style.css and add images directory

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const heading = document.querySelector('h1')
+// const heading = document.querySelector('h1')
 
-const titleClick = (e) => {
-  console.log(e.target.innerText)
-  e.target.style.color = "black";
-}
+// const titleClick = (e) => {
+//   console.log(e.target.innerText)
+//   e.target.style.color = "black";
+// }
 
-heading.addEventListener('click', titleClick)
+// heading.addEventListener('click', titleClick)
