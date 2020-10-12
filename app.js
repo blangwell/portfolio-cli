859b33620914d4cc8c81489bb67fa1b90bb61040
blangwell <b.langwell@outlook.com>
2020-10-12 11:19:12 -0700

    [portfolio] Add contact.html and projects.html, add placeholder text

---
+++ app.js
@@ -0,0 +1,8 @@
+const heading = document.querySelector('h1')
+
+const titleClick = (e) => {
+  console.log(e.target.innerText)
+  e.target.style.color = "black";
+}
+
+heading.addEventListener('click', titleClick)
