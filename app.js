a9c2acae90fa90194fb0dd1e78ea6d284228d83f
blangwell <b.langwell@outlook.com>
2020-10-14 11:25:53 -0700

    [blurb] Add hidden class to blurb div, and call typing method on portfolioCat

--- app.js
+++ app.js
@@ -1,19 +1,11 @@
-// const heading = document.querySelector('h1')
-
-// const titleClick = (e) => {
-//   console.log(e.target.innerText)
-//   e.target.style.color = "black";
-// }
-
-// heading.addEventListener('click', titleClick)
-
 const portfolioTxt = "cat portfolio.txt"
 const navTxt = "cat navigation.txt"
-// const cliCommand = document.querySelector('.cli-command');
+
 const portfolioCat = document.getElementById('portfolio-cat');
 const navCat = document.getElementById('nav-cat');
 
-const navLinks = document.querySelector('.nav-links')
+const navLinks = document.querySelector('.nav-links');
+const blurb = document.querySelector('.blurb');
 
 const showElem = (elem) => {
   console.log('show elem')
@@ -38,7 +30,10 @@ window.addEventListener('DOMContentLoaded', (e) => {
   // conditional logic to target different pages
   if (document.title == 'Portfolio') { 
     typing(navCat, navTxt, 0, navLinks)
-    // typing(portfolioCat, portfolioTxt, 0)
+    typing(portfolioCat, portfolioTxt, 0, blurb)
     
+  }
+  if (document.title == 'Projects') {
+    console.log('projects')
   }
 })
