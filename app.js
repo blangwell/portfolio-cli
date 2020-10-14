286bb0393b9dcfaf234b3c276060bab8fe0341d9
blangwell <b.langwell@outlook.com>
2020-10-14 10:29:44 -0700

    [index] Add typing method to index.js and test

--- app.js
+++ app.js
@@ -6,3 +6,25 @@
 // }
 
 // heading.addEventListener('click', titleClick)
+
+const portfolioText = "cat portfolio.txt"
+
+// const cliCommand = document.querySelector('.cli-command');
+const portfolioCLI = document.getElementById('portfolioCLI');
+
+let i = 0;
+const typing = () => {
+
+  if (i < portfolioText.length) {
+    if (portfolioText.charAt(i) === ' ') {
+      portfolioCLI.innerHTML += "&nbsp;"
+    }
+    portfolioCLI.innerText += portfolioText.charAt(i);
+    i++; 
+    setTimeout(typing, 100)
+  }
+}
+
+window.addEventListener('DOMContentLoaded', (e) => {
+  typing()
+})
